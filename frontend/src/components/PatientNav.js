f0db06c8930ba0b870de0b03e18d878ff9e1cb12
missingpizaslice <gabrielt2609@gmail.com>
2023-10-19 17:50:08 +0800

    updated navbar

--- frontend/src/components/PatientNav.js
+++ frontend/src/components/PatientNav.js
@@ -5,6 +5,8 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Button, Typography } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function PatientNav() {
   const isLoggedIn = !!localStorage.getItem("doctorData");
@@ -16,18 +18,42 @@ export default function PatientNav() {
     localStorage.removeItem("userData");
   };
 
+  const handleHomeClick = () => {
+    if (isLoggedIn) {
+      if (
+        JSON.parse(localStorage.getItem("doctorData"))["staffType"] == "admin"
+      ) {
+        navigate("/admindashboard");
+      } else {
+        navigate("/doctorDashboard");
+      }
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {isLoggedIn ? (
+            <Button onClick={handleHomeClick} color="inherit">
+              <HomeIcon />
+            </Button>
+          ) : (
+            <Button onClick={handleHomeClick} color="inherit">
+              <ArrowBackIcon />
+            </Button>
+          )}
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
             Pain Analysis
           </Typography>
           {isLoggedIn && (
-            <Button
-              onClick={handleLogout}
-              color="inherit"
-            >
+            <Button onClick={handleLogout} color="inherit">
               Logout
             </Button>
           )}
